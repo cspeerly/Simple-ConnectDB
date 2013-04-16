9cf7f7cf518b9fc7c2edf645d20d1cf1f1c35eee
Paul Brown <paul90brown@gmail.com>
2013-04-16 14:14:23 -0500

    Improve compatibility by adding meta tags
    This addition makes it work in IE7

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<title>Simple-ConnectDB</title>
-	 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?php
 
 require_once 'includes/db_Config.php';
